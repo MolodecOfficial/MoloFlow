b5c32e100ce3e8b6e269e6fcf6faeac6314cecc6
Molodec Official <maksimkq1990@mail.ru>
2026-03-05 00:31:36 +0500

    first mega commit!!!

---
+++ server/api/users/login.post.ts
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+
+import User from "~~/server/models/user.model";
+
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event)
+    const { name, password } = body;
+
+    if (!body) {
+        return createError({ statusCode: 400, message: 'Ошибка: тело запроса не может быть пустым.' });
+    }
+
+    // Найти пользователя по email и выбрать пароль
+    const user = await User.findOne({ name: body.name }).select('+password');
+
+    if (!user) {
+        return createError({ statusCode: 401, message: 'Неверное имя или пароль.' });
+    }
+
+    // Проверка пароля
+    const isPasswordCorrect = await bcrypt.compare(body.password, user.password);
+
+    if (!isPasswordCorrect) {
+        return createError({ statusCode: 401, message: 'Неправильный пароль.' });
+    }
+    return {
+        message: `${user.name}`,
+        user: {
+            _id: user._id,
+            name: user.name,
+            role: user.role,
+        }
+    }
+})
